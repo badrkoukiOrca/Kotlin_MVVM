a7f92ceb5c3467901595b2936aca4f8e3a89320e
hma <hma@orcaformation.fr>
2019-06-07 16:25:17 +0100

    added MVVM architecture to project

--- app/src/main/java/com/example/kotlin_mvvm/Model/MyName.kt
+++ app/src/main/java/com/example/kotlin_mvvm/Model/MyName.kt
@@ -1,2 +1,2 @@
-package com.example.kotlin_mvvm
+package com.example.kotlin_mvvm.Model
 class MyName (var name: String="",var lastname:String="")
--- app/src/main/java/com/example/kotlin_mvvm/View/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_mvvm/View/MainActivity.kt
@@ -1,37 +1,31 @@
-package com.example.kotlin_mvvm
+package com.example.kotlin_mvvm.View
 
-import android.content.Context
 import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.inputmethod.InputMethodManager
+import com.example.kotlin_mvvm.Model.MyName
+import com.example.kotlin_mvvm.R
 
 class MainActivity : AppCompatActivity() {
-
     private lateinit var binding: com.example.kotlin_mvvm.databinding.ActivityMainBinding
-
-    private val myName: MyName = MyName("Aleks Haecky")
+    private val myName: MyName = MyName("Kouki", "Badr")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         binding.myName = myName
         binding.doneButton.setOnClickListener {
-            addNickname(it)
+            com.example.kotlin_mvvm.ViewModel.addNickname(it, binding)
         }
     }
-    private fun addNickname(view: View) {
+    /*private fun addNickname(view: View) {
         binding. apply {
             myName?.lastname = nicknameEdit.text.toString()
             invalidateAll()
-            nicknameEdit.visibility = View.GONE
-            doneButton.visibility = View.GONE
             nicknameText.visibility = View.VISIBLE
         }
-
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
-    }
+    }*/
 }
